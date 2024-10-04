ffa7961980c48e73766b729a72796448c9772708
ramseslaurel <laurelri@sonoma.edu>
2024-10-03 17:47:55 -0700

    Update shapes.js

--- Assignment-2/shapes.js
+++ Assignment-2/shapes.js
@@ -7,10 +7,33 @@ function init() {
     if (!gl) { alert("Your Web browser doesn't support WebGL 2\nPlease contact Dave"); }
 
     // Add initialization code here
-}
+
+    gl.clearColor(0.2, 0.2, 0.2, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    let sphere = new Sphere(gl, 36, 18);
+    let ms = new MatrixStack();
+    let angle = 0.0;
+    
 
 function render() {
     // Add rendering code here
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+                    angle += 1.0;
+                    angle %= 360.0;
+
+                    ms.push();
+                    ms.rotate(angle, [0, 1, 0]);
+                    ms.scale(0.9);
+                    sphere.MV = ms.current();
+                    sphere.draw();
+                    ms.pop();
+
+                    requestAnimationFrame(render);
+                };
+
+    render();
 }
 
 window.onload = init;
