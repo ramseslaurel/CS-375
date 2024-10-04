5508a6f183519b3ea406c39151c2459dd95c56bc
ramseslaurel <laurelri@sonoma.edu>
2024-10-03 18:35:27 -0700

    Update shapes.js

--- Assignment-2/shapes.js
+++ Assignment-2/shapes.js
@@ -10,26 +10,50 @@ function init() {
 
     gl.clearColor(0.2, 0.2, 0.2, 1.0);
     gl.enable(gl.DEPTH_TEST);
+
+    //sphere
     let sphere = new Sphere(gl, 36, 18);
     let ms = new MatrixStack();
-    let angle = 0.0;
-    
+    let sphere_angle = 0.0;
+    //
+
+    //Tetrahedron
+
+    let tetrahedron = new Tetrahedron(gl, 40, 20);
+    let ms = new MatrixStack();
+    let tetrahedron_angle = 0.0;
+    //
 
 function render() {
     // Add rendering code here
 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    angle += 1.0;
-    angle %= 360.0;
+    // sphere
+    sphere_angle += 3.0;
+    sphere_angle %= 360.0;
 
     ms.push();
-    ms.rotate(angle, [0, 1, 0]);
-    ms.scale(.50,.25,.50);
-    ms.translate(0.5,-0.5,0.0);
+    ms.rotate(sphere_angle, [1, 1, 1]);
+    ms.scale(.50,.50,.50);
+    ms.translate(0.5,-0.5,0.5);
     sphere.MV = ms.current();
-    sphere.color = vec4(0.5, 0.0, 0.5, 1.0);
+    sphere.color = vec4(0.5, 0.0, 0.5, 1.0); //purple
     sphere.draw();
+    ms.pop();
+    //
+
+    // Tetrahedron
+    tetrahedron_angle += 5.0;
+    tetrahedron_angle %= 360.0;
+
+    ms.push();
+    ms.rotate(stetrahedron_angle, [1, 0, 0]);
+    ms.scale(.70,.25,1);
+    ms.translate(0.5,0,0.5);
+    tetrahedron.MV = ms.current();
+    tetrahedron.color = vec4((1.0, 0.0, 0.0, 1.0);
+    tetrahedron.draw();
     ms.pop();
 
     requestAnimationFrame(render);
