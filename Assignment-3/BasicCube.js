8ebca2741a302830a584bf074344d9235a43f725
ramseslaurel <laurelri@sonoma.edu>
2024-10-30 10:12:51 -0700

    Update BasicCube.js

--- Assignment-3/BasicCube.js
+++ Assignment-3/BasicCube.js
@@ -8,10 +8,10 @@
 class BasicCube {
     constructor(gl, vertexShader, fragmentShader) {
 
-        // let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+         let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
 
         this.draw = () => {
-            // program.use();
+             program.use();
         };
     }
 };
